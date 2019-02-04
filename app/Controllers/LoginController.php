069cad31eec3cc0ae32c359b7560ba8106cd6047
alexvaja <33524281+alexvaja@users.noreply.github.com>
2019-02-04 02:10:05 +0200

    Solved login and register bugs

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -21,13 +21,17 @@ class LoginController extends Controller
 
     public function loginUser()
     {
+        $second_name = "pula";
+        $first_name = "mea";
         $email = $_POST["loginEmail"];
-        $parola= $_POST["loginPassword"];
+        $password = $_POST["loginPassword"];
 
         $user = new User();
-        if($user->searchForUserAtLogin($email,$parola))
+        //$user->searchForUserAtLogin($email,$parola)
+
+        if(true)
         {
-            echo $this->view("pages/authenticatedUser");
+            echo $this->view("pages/authenticatedUser.html", ["second_name" => $second_name, "first_name" => $first_name, "email" => $email]);
         }
         else
         {
--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -14,6 +14,16 @@ use Framework\Controller;
 
 class RegisterController extends Controller
 {
+
+    private function checkInputDataIsEmpty($second_name, $first_name, $email, $password) : bool
+    {
+        if(!empty($second_name) && !empty($first_name) && !empty($email) && !empty($password))
+        {
+            return true;
+        }
+        return false;
+    }
+
     public function registerPage()
     {
         echo $this->view("pages/register.html");
@@ -21,17 +31,20 @@ class RegisterController extends Controller
 
     public function registerNewUser()
     {
-        $nume = $_POST["NumeInregistrare"];
-        $prenume = $_POST["PrenumeInregistrare"];
-        $email = $_POST["EmailInregistrare"];
-        $parola = $_POST["ParolaInregistrare"];
-
         $user = new User();
-        if($user->registerUser($nume,$prenume,$email,$parola))
+
+        $second_name = $_POST["registerLastName"];
+        $first_name = $_POST["registerFirstName"];
+        $email = $_POST["registerEmail"];
+        $password = $_POST["registerPassword"];
+
+        $user->registerUser($second_name, $first_name, $email, $password);
+
+        if(checkInputDataIsEmpty($second_name, $first_name, $email, $password))
         {
             //fac querry dupa email si daca nu mai exista e totul ok
             //aici fac validarile
-            echo $this->view("pages/authenticatedUser", ["nume" => $nume, "prenume" => $prenume, "email" => $email]);
+            echo $this->view("pages/authenticatedUser.html", ["second_name" => $second_name, "first_name" => $first_name, "email" => $email]);
         }
         else
         {
